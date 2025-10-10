2b9758c76328c59cfb3b8e9db4389d05e2dc4242
Max Duval <max.duval@hometrack.com>
2025-10-10 11:40:16 +0100

    feat: initial Todo setup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# immutability
-On the merits of Immutability
+# On the Merits of Immutability
+
+Presented at Hometrack/Zoopla offices on Wed 15 Oct 2025.
+
+We’ll explore why React loves immutable data structures.
+
+## Bad
+
+There’s a but in here!
+
+## Good
+
+There no longer is a bug, thanks to TypeScript’s `ReadonlyArray<T>`!
+
+## Better
+
+Some data structures are better suited to certain operations, like `Set`!
+
+## Best
+
+For performance, you can even leverage bitmasks!
---
+++ src/Todos.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import logo from "./assets/hometrack.svg";
+import { uuidv7 } from "uuidv7";
+
+const all_status = ["todo", "doing", "done"] as const;
+
+interface Todo {
+  /** must be unique! */
+  id: string;
+  title: string;
+  status: (typeof all_status)[number];
+  assignee: string[];
+}
+
+export function Todos() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  return (
+    <>
+      <header>
+        <a href="https://www.hometrack.com" target="_blank">
+          <img src={logo} className="logo" alt="Hometrack logo" />
+        </a>
+      </header>
+      <main>
+        <h1>On the merits of Immutability</h1>
+        <h2>Todo List</h2>
+        <button
+          onClick={() => {
+            const title = prompt("Title?")?.trim();
+            if (!title) return;
+            const id = uuidv7();
+            setTodos((todos) => {
+              todos.push({ id, title, status: "todo", assignee: [] });
+              return todos;
+            });
+          }}
+        >
+          add
+        </button>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+          {all_status.map((status) => (
+            <div key={status}>
+              <h3>
+                {status.at(0)?.toUpperCase()}
+                {status.slice(1)}
+              </h3>
+              <ul>
+                {todos.flatMap((todo) =>
+                  todo.status === status
+                    ? [<li key={todo.id}>{todo.title}</li>]
+                    : [],
+                )}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
